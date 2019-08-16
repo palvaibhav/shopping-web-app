dc894b62fc534085f7c63ed49d7f6bab2c1c74ab
palvaibhav <vaibhav171997@gmail.com>
2019-08-16 19:34:25 +0530

    Project till module 11

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'shop',
-    password: 'lenovob490'
+const sequelize = new Sequelize("shop", "root", "lenovob490", {
+  dialect: "mysql",
+  host: "localhost"
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
