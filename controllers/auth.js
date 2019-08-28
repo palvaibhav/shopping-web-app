437ef9df15361d3df07a9ee7a9d1e96253c707e8
palvaibhav <vaibhav171997@gmail.com>
2019-08-28 21:23:18 +0530

    Added nodemalier & sendgrid for sending mails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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"
+    }
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -85,6 +96,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect("/login");
+          return transporter.sendMail({
+            to: email,
+            from: "shop@shoppin.com",
+            subject: "Signup succeeded!",
+            html: "<h1>You successfully signed up!</h1>"
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
